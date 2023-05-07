0589fa9e05b24c9ca9b1f8e9c4387efdd13e862c
Cody Robinson <crobinson@skillpath.com>
2023-05-07 14:38:23 -0500

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-YG376LSLBR"></script>
                 <script>
                     window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
+                    function gtag(){dataLayer.push(arguments)}
                     gtag('js', new Date());
 
                     gtag('config', 'G-YG376LSLBR');
