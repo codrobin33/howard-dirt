10e0c1cf1c19368413e42cd548081b2f08482991
Cody Robinson <crobinson@skillpath.com>
2023-05-07 13:27:24 -0500

    recaptcha

--- components/Contact.js
+++ components/Contact.js
@@ -76,7 +76,13 @@ export default function Contact() {
                         </dl>
                     </div>
                 </div>
-                <form name="contact" method="POST" data-netlify="true" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+                <form
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    data-netlify-recaptcha="true"
+                    className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+                >
                     <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
                         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                             <div>
