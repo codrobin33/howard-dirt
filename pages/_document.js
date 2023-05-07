22df4cccadc51dce2d0466295b74c54a65934d80
Cody Robinson <crobinson@skillpath.com>
2023-05-07 14:40:31 -0500

    gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,25 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
         <Html>
             <Head>
                 <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-YG376LSLBR"></script>
-                <script>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-YG376LSLBR"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
                     window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments)}
+                    function gtag(){window.dataLayer.push(arguments);}
                     gtag('js', new Date());
 
                     gtag('config', 'G-YG376LSLBR');
-                </script>
+                    `}
+                </Script>
             </Head>
             <body>
                 <Main />
