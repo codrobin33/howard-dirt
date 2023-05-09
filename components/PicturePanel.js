e49f1e2c78365a14d547f85970c4a185aec0c2a4
Cody Robinson <crobinson@skillpath.com>
2023-05-08 22:19:45 -0500

    girl

--- components/PicturePanel.js
+++ components/PicturePanel.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+import girl from '../public/girl.webp';
 import truck from '../public/truck.webp';
 
 export default function PicturePanel() {
@@ -34,11 +35,16 @@ export default function PicturePanel() {
                                     </p>
                                 </blockquote>
                                 <figcaption className="mt-6 flex gap-x-4 text-sm leading-6">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
-                                        alt=""
-                                        className="h-6 w-6 flex-none rounded-full"
-                                    />
+                                    <div className="h-6 w-6 flex-none rounded-full relative">
+                                        <Image
+                                            src={girl}
+                                            layout="fill"
+                                            objectFit="cover"
+                                            objectPosition="center"
+                                            alt="Review Headshot"
+                                            style={{ borderRadius: '9999px' }}
+                                        />
+                                    </div>
                                     <div>
                                         <span className="font-semibold text-gray-900">Cassandra Robinson</span>
                                     </div>
