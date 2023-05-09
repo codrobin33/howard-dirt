a7a2ba08d623e967e3b265bc292373d9333b0fb4
Cody Robinson <crobinson@skillpath.com>
2023-05-08 22:36:20 -0500

    placeholder

--- components/Header.js
+++ components/Header.js
@@ -24,6 +24,7 @@ export default function Header() {
                         layout="fill"
                         objectFit="cover"
                         objectPosition="center"
+                        placeholder="blur"
                         priority
                     />
                 </div>
